90e513c1e925b26c696d1545d0d9cfb4c7aead24
toh leng wee <tohlengwee@hotmail.com>
2017-03-23 16:26:00 +0800

    Added bootstrap and login controller

---
+++ Horus/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Horus.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
